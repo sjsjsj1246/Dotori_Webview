08ecd92b484876de678d54309a1b8fae2799ca09
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-04-22 15:43:47 +0900

    init project

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-controls",
+        "@storybook/addon-knobs",
+        "storybook-addon-styled-component-theme/dist/register",
+    ],
+    webpackFinal: async (config) => {
+        return config;
+    },
+};
