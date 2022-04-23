aeb01d712540b4c3a12bd8a96aa1f13b9ad66507
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-04-23 22:20:31 +0900

    Fix: Test 재작성

--- src/components/bottomTab/index.tsx
+++ src/components/bottomTab/index.tsx
@@ -1,16 +1,15 @@
+import React from 'react';
 import { TabBar } from 'antd-mobile';
 import {
   TabBarItemProps,
   TabBarProps,
 } from 'antd-mobile/es/components/tab-bar';
-import { TabBarItem } from 'antd-mobile/es/components/tab-bar/tab-bar';
-import React from 'react';
 import styled from 'styled-components';
 import { color } from 'styled-system';
 
 const StyledBottomTab = styled(TabBar)``;
 
-const StyledBottomTabItem = styled(TabBarItem)``;
+const StyledBottomTabItem = styled(TabBar.Item)``;
 
 export const BottomTab: React.FC<TabBarProps> & {
   Item: React.FC<TabBarItemProps>;
