fbffe890ef434dcb4199b2051afe2049c12e7083
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-04-27 20:30:00 +0900

    Feat: add font face

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -17,7 +17,7 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
     margin: 0;
     padding: 0;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-family: Bold, -apple-system, sans-serif;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -28,6 +28,24 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     height: 100%;
     background: white;
+  }
+
+  @font-face {
+    font-family: "light";
+    font-weight: 300;
+    src: url("/fonts/FuturaStdLight.otf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "medium";
+    font-weight: 500;
+    src: url("/fonts/FuturaStdMedium.otf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "bold";
+    font-weight: 700;
+    src: url("/fonts/FuturaStdBold.otf") format("truetype");
   }
 `;
 
